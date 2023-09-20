1cfd56a03821c3e77caa8086e9a44141d7153f8d
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-09-20 17:43:38 +0900

    뼈대 완성

--- character_runs.py
+++ character_runs.py
@@ -1,7 +1,23 @@
-from pico2d import *
 import math
+from pico2d import *
 
 open_canvas()
 
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def run_circle():
+	print("CIRCLE")
+	pass
+
+def run_rectangle():
+	print("RECTANGLE")
+	pass
+
+
+while True:
+	run_circle()
+	run_rectangle()
+	break
 
 close_canvas()
